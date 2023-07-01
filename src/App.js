8a7e2590ef362a249b52eefea778b7a0fd2acb94
Colin Huber <cbhuber@gmail.com>
2023-07-01 11:55:03 -0600

    feat: loading questions from JSON-server API.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import Header from "./Header";
 import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  // Valid statuses: 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error(`Action ${action.type} unknown`);
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  });
+
   return (
     <div className="app">
       <Header />
