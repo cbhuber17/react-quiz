85cbabdd2efada49a99628bfb430092011b354af
Colin Huber <cbhuber@gmail.com>
2023-07-02 17:01:40 -0600

    feat: moving to next question.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 import { useEffect, useReducer } from "react";
 
 const initialState = {
@@ -39,6 +40,9 @@ function reducer(state, action) {
             : state.points,
       };
 
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
     default:
       throw new Error(`Action ${action.type} unknown`);
   }
@@ -71,11 +75,14 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
