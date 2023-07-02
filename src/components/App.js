ef768b5d3b97b52fd402ef1cf800cfd7eab1a4c4
Colin Huber <cbhuber@gmail.com>
2023-07-02 16:49:15 -0600

    feat: handling answers.

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,9 @@ const initialState = {
 
   // Valid statuses: 'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
-
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -26,13 +27,25 @@ function reducer(state, action) {
     case "start":
       return { ...state, status: "active" };
 
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
     default:
       throw new Error(`Action ${action.type} unknown`);
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -57,7 +70,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,14 @@
 import Options from "./../components/Options.js";
 
-function Question({ question }) {
+function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question}></Options>
+      <Options
+        question={question}
+        dispatch={dispatch}
+        answer={answer}
+      ></Options>
     </div>
   );
 }
