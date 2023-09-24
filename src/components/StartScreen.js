d3b7f46b2149e15387f45c248602c9ceb79b19bd
Colin Huber <cbhuber@gmail.com>
2023-09-23 18:34:28 -0600

    fix: number questions not displaying properly.

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,11 @@
 import { useQuizzes } from "../contexts/QuizContext";
 
 function StartScreen() {
-  const { numQuestions, dispatch } = useQuizzes();
+  const { questions, dispatch } = useQuizzes();
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
-      <h3>{numQuestions} questions to test your React mastery</h3>
+      <h3>{questions.length} questions to test your React mastery</h3>
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "start" })}
